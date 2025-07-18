e47ba44f6641ee35fd48d7be25fd708eb0ff524e
adam <chihaadam137@gmail.com>
2025-07-18 08:56:31 +0100

    simple fixes

--- src/components/finished.tsx
+++ src/components/finished.tsx
@@ -15,11 +15,11 @@ function Finished() {
   const {points}=usePoints();
   return (
     <div className="w-fit text-white bg-[hsl(0,0%,100%,0.05)] flex flex-col items-center gap-2 p-2 rounded-xl">
-      <img src='src/assets/congrats.png'></img>
-      <div className="w-4/5 text-center flex flex-col gap-4"> 
-      <h1 className=' text-2xl'>Congrats! you completed the quiz </h1>
+      <img src='./assets/congrats.png'></img>
+      <div className="flex flex-col w-4/5 gap-4 text-center"> 
+      <h1 className='text-2xl '>Congrats! you completed the quiz </h1>
       <p>You answer {points}/10 correctly</p>
-      <button className="gradiant text-xl py-2 px-14 rounded-lg hover:brightness-110 transition-all duration-300 ease-in-out cursor-pointer my-10" onClick={handleRestart}>Play again</button>
+      <button className="py-2 my-10 text-xl transition-all duration-300 ease-in-out rounded-lg cursor-pointer gradiant px-14 hover:brightness-110" onClick={handleRestart}>Play again</button>
       </div>
     </div>
   )
